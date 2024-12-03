8df785369a947c31c77b0770032a9b876627d8da
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-12-03 23:08:44 +0530

    Tailwind Configure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className='text-center text-3xl'>JMC CAMPUS CONNECT</div>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default 
+{
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}", 
+	],
+	theme: {
+	  extend: {},
+	},
+	colors: {
+	  'bharath-blue': '#0073e6',
+	},
+	plugins: [],
+}
+  
