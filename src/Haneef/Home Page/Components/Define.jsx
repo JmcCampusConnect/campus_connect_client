241d12d7442eb62ac0e4b8f74639d09db118e4a9
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-12-09 15:15:15 +0530

    App File

--- src/Haneef/Home Page/Components/Define.jsx
+++ src/Haneef/Home Page/Components/Define.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import JCC from '../../assets/jmclogo.png';
+import JCC from '../../../assets/jmclogo.png';
 
 function Define() 
 {
