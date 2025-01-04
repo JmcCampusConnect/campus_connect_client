268c1bc7d9daa621b12b4e3383bab12b43d34e91
therasak <abdulrasak4567@gmail.com>
2025-01-04 13:54:40 +0530

    Login Page

--- src/Haneef/Sign Login/Components/Login.jsx
+++ src/Haneef/Sign Login/Components/Login.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faSignInAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Login({ activeTab }) 
 {
     return (
         <>
-            {activeTab === 'Login' && (
-                <div className='flex flex-col gap-9 px-16 py-11'>
-                    <span className='text-2xl font-bold text-blue-800 text-center font-cambria'>LOGIN INTO YOUR ACCOUNT</span>
-                    <input
-                        className='bg-transparent border-[1.5px] rounded-md border-black py-2.5 px-3 focus:outline-none'
-                        placeholder='Username'
-                    />
-                    <input
-                        className='bg-transparent border-[1.5px] rounded-md border-black py-2.5 px-3 focus:outline-none'
-                        placeholder='Password'
-                    />
+            {activeTab === 'Log In' && (
+                <div className='flex flex-col justify-center h-screen items-center p-16 px-28 gap-12'>
+                    <span className='text-3xl font-bold text-white text-center font-cambria'>WELCOME BACK !</span>
+                    <span className='text-lg text-slate-200'>Log In to continue your journey with us.</span>
+                    <div className='flex border-b-2 w-full'>
+                        <div className='w-[7%] h-12 flex justify-center items-center'>
+                            <FontAwesomeIcon icon={faUser} className="text-md text-white" />
+                        </div>
+                        <div className='w-[93%] p-3'>
+                            <input
+                                input='text'
+                                className='bg-transparent focus:outline-none text-white text-md placeholder:text-white'
+                                placeholder='USER NAME'
+                            />
+                        </div>
+                    </div>
+                    <div className='flex border-b-2 w-full'>
+                        <div className='w-[7%] h-12 flex justify-center items-center'>
+                            <FontAwesomeIcon icon={faLock} className="text-md text-white" />
+                        </div>
+                        <div className='w-[93%] p-3'>
+                            <input
+                                type='psw'
+                                className='bg-transparent focus:outline-none text-white text-md placeholder:text-white'
+                                placeholder='PASSWORD'
+                            />
+                        </div>
+                    </div>
                     <button
-                        className="w-full p-2.5 bg-blue-700 text-white text-lg font-semibold rounded-md shadow-md hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center"
-                        // onClick={handleLogin}
+                        className="w-full bg-orange-500 p-2.5 mt-5 text-white text-md font-semibold rounded-md shadow-md hover:bg-orange-600 focus:outline-none flex items-center justify-center hover:shadow-slate"
                     >
-                        <FontAwesomeIcon icon={faLock} className="mr-2" />
-                        <span className='font-cambria text-xl mt-0.5'>LOGIN</span>
+                        <FontAwesomeIcon icon={faSignInAlt} className="mr-3" />
+                        <span className='font-cambria text-xl'>LOGIN</span>
                     </button>
                 </div>
             )}
--- src/Haneef/Sign Login/Main.jsx
+++ src/Haneef/Sign Login/Main.jsx
@@ -1,37 +1,30 @@
-import React, { useState } from 'react';
+import React, { act, useState } from 'react'
 import Login from './Components/Login';
-import Sign from './Components/Sign'
+import Sign from './Components/Sign';
+import JmcLogo from '../../assets/JmcLogo.png'
 
 function Main() 
 {
-    const [activeTab, setActiveTab] = useState('Login');
+    const [ activeTab, setActiveTab ] = useState('Log In')
 
-    const switchTab = (value) => {
+    const switchTab = () => {
 
-        setActiveTab(value)
+        setActiveTab((prev) => (activeTab === 'Log In' ? 'Sign Up' : 'Log In'))
     }
 
-    return ( 
-        <div className='w-full h-screen flex px-32 py-24 bg-gradient-to-l from-blue-700 to-blue-400'>
-            <div className='w-full flex'>
-                <div className='w-1/2 bg-white'>
-                </div>
-                <div className='w-1/2 bg-white border-l-[1px] border-black p-0'>
-                    <button
-                        className={ activeTab === 'Login' ? 'w-1/2 p-4 bg-blue-700 uppercase text-white font-cambria text-lg font-bold' : 'w-1/2 p-4 bg-white uppercase text-black font-cambria text-lg font-bold border-none' }
-                        onClick={() => switchTab('Login')}
-                    >
-                        Log In
-                    </button>
-                    <button
-                        className={ activeTab === 'Sign In' ? 'w-1/2 p-4 bg-blue-700 uppercase text-white font-cambria text-lg font-bold' : 'w-1/2 p-4 bg-white uppercase text-black font-cambria text-lg font-bold' }
-                        onClick={() => switchTab('Sign In')}
-                    >
-                        Sign In
-                    </button>
-                    <Login activeTab={activeTab} />
-                    <Sign  activeTab={activeTab} />
-                </div>
+    return (
+        <div className='w-full h-screen flex'>
+            <div className="w-1/2 bg-slate-200 flex flex-col justify-center items-center p-10 space-y-8">
+        <img src={JmcLogo} className="w-64 h-auto" alt="JMC Logo" />
+        <button
+          onClick={switchTab}
+          className="border border-orange-500 w-60 py-3 font-cambria text-xl text-black rounded-lg font-medium uppercase shadow-lg hover:bg-orange-500 hover:text-white transition duration-300 ease-in-out"
+        >
+          {activeTab === 'Log In' ? 'Sign Up' : 'Log In'}
+        </button>
+      </div>
+            <div className='w-1/2 bg-gradient-to-b from-blue-600 to-blue-800 h-screen'>
+                {activeTab === 'Log In' ? <Login activeTab={activeTab} /> : <Sign activeTab={activeTab} />}
             </div>
         </div>
     )
